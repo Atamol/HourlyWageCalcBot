1f376234e797b88b0220ae02e01bfff7669ed506
Atamol <35617831+Atamol@users.noreply.github.com>
2024-11-08 05:21:43 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,8 +12,6 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 DISCORD_BUTTON_CH = os.getenv('DISCORD_BUTTON_CH')
 DISCORD_LOG_CH = os.getenv('DISCORD_LOG_CH')
 
-print("Bot is starting up...")
-
 class bot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
